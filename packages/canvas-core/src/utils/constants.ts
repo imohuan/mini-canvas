87a9aee409b1a0a9046cb63367f39d4ff2f44b4a
imohuan <2652293750@qq.com>
2026-07-04 21:10:41 +0800

    refactor(canvas-core): add unified constants for default node sizes

---
+++ packages/canvas-core/src/utils/constants.ts
@@ -0,0 +1,11 @@
+﻿/** 节点默认卡片尺寸（宽 x 高，像素） */
+export const DEFAULT_NODE_SIZE = {
+  width: 256,
+  height: 256,
+} as const
+
+/** 性能面板默认节点估算尺寸 */
+export const DEFAULT_PERF_NODE_SIZE = {
+  width: 180,
+  height: 120,
+} as const
